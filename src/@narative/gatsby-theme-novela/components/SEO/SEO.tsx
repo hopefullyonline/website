f233480a95051874067f37c066039c28abc31ba9
Anatoli Nicolae <desk@anatolinicolae.com>
2020-06-28 22:51:04 +0200

    Upgrade packages

--- src/@narative/gatsby-theme-novela/components/SEO/SEO.tsx
+++ src/@narative/gatsby-theme-novela/components/SEO/SEO.tsx
@@ -63,6 +63,14 @@ const themeUIDarkModeWorkaroundScript = [
         }
       } catch (e) {}
     })();
+    (function(d) {
+        var config = {
+            kitId: 'igs3orr',
+            scriptTimeout: 3000,
+            async: true
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+    })(document);
   `,
     },
 ];
@@ -139,7 +147,6 @@ const SEO: React.FC<HelmetProps> = ({
             script={themeUIDarkModeWorkaroundScript}
             meta={metaTags}
         >
-            <link href="https://use.typekit.net/igs3orr.css?display=swap" rel="stylesheet"/>
             <link rel="canonical" href={canonicalUrl}/>
             {children}
         </Helmet>
